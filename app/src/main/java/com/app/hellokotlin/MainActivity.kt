0458013a607c2f8bd0a232c4bcf66f807d9dad2a
Junaid <junaidkodoor@gmail.com>
2018-04-10 04:10:39 +0530

    Initial commit

---
+++ app/src/main/java/com/app/hellokotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.app.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener{ toast("Welcome to kotlin")}
+
+    }
+    
+}
